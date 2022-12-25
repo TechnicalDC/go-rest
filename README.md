348e58ba7f8e9493d91f2f4c64e883d0353eb557
Dilip Chauhan <chauhandilip305@gmail.com>
2022-12-25 16:24:17 +0530

    Updated on 25 December 2022

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # go-rest
 REST api in golang
+
+Nothing just demo project in order to learn building api in golang.
+
+```json
+{
+	"ip" : "192.168.42.1",
+	"port" : 22,
+	"dbHostname" : "192.168.42.1",
+	"dbPort" : 22,
+	"dbUser" : "dilip",
+	"dbPass" : "123"
+}
+```
--- main.go
+++ main.go
@@ -1,18 +1,75 @@
 // Rest api program
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
-func hello() {
-	return 
+type User struct {
+	Name		string
+	City		string
+	Dob		string
+	Email 	string
+	Password string
+	Active	bool
+}
+
+type Users struct {
+	User		[]User
+}
+
+type Config struct {
+	Ip			string
+	Port		string
+	DbIp		string
+	DbPort	string
+	DbUser	string
+	DbPass	string
 }
 
 func main() {
-	var name string = "DC"
+	content, err := ioutil.ReadFile("./config.json")
+	var payload Config
 
-	if len(name) != 0 {
-		fmt.Println("more than 0")
+	if err != nil {
+		log.Fatal("Error when opening file: ", err)
 	}
 
-	fmt.Printf("Hello %s", name)
+	if json.Valid(content) {
+		err = json.Unmarshal(content, &payload)
+		if err != nil {
+			log.Fatal("Error during Unmarshal(): ", err)
+		}
+	} else {
+		fmt.Println("Invalid JSON!")
+	}
+
+	handleRequest(payload)
+}
+
+func handleRequest(config Config) {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":" + config.Port, nil))
+}
+
+func homePage(response http.ResponseWriter, request *http.Request) {
+
+	var reader []byte;
+
+	body, err := request.Body.Read(reader)
+
+	if err != nil {
+		fmt.Println("Error")
+	}
+
+	// body,_ := io.ReadCloser(request.Body.Read(reader)).Read(reader)
+
+	fmt.Println(reader, body)
+
+	io.WriteString(response, "Hello, world!\n")
 }
